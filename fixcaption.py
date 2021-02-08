f3b73a097bb1a819327e1755deeb0723b991b4d3
Johan Van den Brande <johan@vandenbrande.com>
2021-02-08 10:53:46 +0100

    Fix caption to img conversion

--- fixcaption.py
+++ fixcaption.py
@@ -8,17 +8,19 @@ class FixCaption(Preprocessor):
     def run(self, lines):
         out = []
         for line in lines:
-            m = re.search("\\[caption.*\\]", line)
+            m = re.search("\\\\\\[caption.*\\]", line)
             if m:
-                print("--> CAPTION DETECTED")
+                # print("--> CAPTION DETECTED")
+                # print(line)
                 caption = m.group(0)
-                print(caption)
+                # print(caption)
                 mc = re.search("](.+)\\\\\[", caption)
                 malformedlink = mc.group(1)
                 mf = re.search("^\!\[\]\((.+?)\)\s+(.+)$", malformedlink)
                 image = '![{desc}]({link} "{desc}")'.format(desc=mf.group(2), link=mf.group(1))
-                print(image)
-                out.append(line.replace(caption, image))
+                new_line = line.replace(caption, image)
+                # print(new_line)
+                out.append(new_line)
             else:
                 out.append(line)
         return out
